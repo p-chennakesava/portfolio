66a9fb6d88d01eece732e56517735f71c8600828
chennakesavapagadala <chennakesavapagadala@gmail.com>
2025-06-10 11:47:49 +0530

    modified urls.py in root

--- KesavaStark/urls.py
+++ KesavaStark/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
     path('', include('frontend.urls')),
+
+    re_path(r'^(?!admin|api|media).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
