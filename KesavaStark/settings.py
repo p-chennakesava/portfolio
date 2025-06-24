1aaec32d4cc6a5ef23ea7e509ae87dc41edb9bf7
chennakesavapagadala <chennakesavapagadala@gmail.com>
2025-06-24 13:55:13 +0530

    update

--- KesavaStark/settings.py
+++ KesavaStark/settings.py
@@ -160,13 +160,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUD_API_KEY'),
     'API_SECRET': os.environ.get('CLOUD_API_SECRET')
 }
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Media files
 # MEDIA_URL = '/media/'
